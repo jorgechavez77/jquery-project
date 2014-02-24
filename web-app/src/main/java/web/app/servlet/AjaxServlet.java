ae9ad1e1db0749275a25e5369fe81fb29c0b444d
jachavez <jachavez@axcess-financial.com>
2014-02-24 09:20:14 -0500

    Add project info

---
+++ web-app/src/main/java/web/app/servlet/AjaxServlet.java
@@ -0,0 +1,30 @@
+package web.app.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AjaxServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		String message = req.getParameter("message");
+		System.out.println(message);
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String message = req.getParameter("message");
+		System.out.println(message);
+		super.doGet(req, resp);
+	}
+
+}
